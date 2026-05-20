9bcf6bcd27404cca429298893d2c46b7694339ff
arturarzumanyan300-afk <arturarzumanyan300@gmail.com>
2026-05-20 09:10:42 +0400

    fix render deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app=require('./app')
 const connectDb=require('./db')
 
 connectDb()
-app.listen(400,()=>{
+const PORT=process.env.PORT || 400
+app.listen(PORT,()=>{
 console.log('Server started on port 400')
 })
